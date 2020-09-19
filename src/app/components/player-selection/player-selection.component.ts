2805a36a2f19f13f19f52325fca26859f96c112a
Cameron Hayes <cdoughayes@gmail.com>
2020-09-19 18:10:22 -0500

    srry if i broke things

--- src/app/components/player-selection/player-selection.component.ts
+++ src/app/components/player-selection/player-selection.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SELECT_PANEL_INDENT_PADDING_X } from '@angular/material/select';
 import { ActivatedRoute } from '@angular/router';
 import { BackendApiService } from 'src/app/services/backend-api.service';
+
 
 @Component({
   selector: 'app-player-selection',
@@ -13,18 +15,42 @@ export class PlayerSelectionComponent implements OnInit {
     currentPlayerSelection: ['', Validators.compose([Validators.required])]
   });
 
-  playerOptions: string[] = ['Harry', 'Ron', 'Hermione', 'Neville'];
+  playerOptions: string[] = [];
+  playerDict = new Map<string, string>();
   constructor(private route: ActivatedRoute, private fb: FormBuilder, private backendApi: BackendApiService) { }
+ 
 
   ngOnInit(): void {
     const gameId = this.route.snapshot.paramMap.get('id');
     console.log(gameId);
+    this.updatePlayerSelection()
   }
 
+  updatePlayerSelection(){
+    const gameId = this.route.snapshot.paramMap.get('id');
+    this.backendApi.getChosenPlayers(gameId).subscribe(game => {
+      //TODO Fix camerons code lol
+      this.playerOptions = []
+      for (var value of game){
+        this.playerDict.set(value.Name,value.id);
+        if(!value.Selected){
+          this.playerOptions.push(value.Name)
+        }
+      }
+    })
+  }
 
+  
   submitPlayerSelection() {
     // tell svc that player is selected
-    this.playerSelectForm.disable();
+    const gameId = this.route.snapshot.paramMap.get('id');
+    this.backendApi.submitChosenPlayers(gameId,this.playerDict.get(this.playerSelectForm.controls.currentPlayerSelection.value)).subscribe(x => {
+      this.updatePlayerSelection();
+      this.playerSelectForm.disable();
+      
+    })
+    
   }
 
 }
+
--- src/app/services/backend-api.service.ts
+++ src/app/services/backend-api.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import { Game } from '../models/game.model';
+import { strict } from 'assert';
+import { JsonPipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -19,4 +21,20 @@ export class BackendApiService {
     const url = 'http://70.120.3.133:5000/game';
     return this.http.post(url, gameConfigs);
   }
+
+  getChosenPlayers(gameId: any): any{
+    const url = 'http://70.120.3.133:5000/game/' + gameId +"/playerOptions" ;
+    return this.http.get(url);
+  }
+  submitChosenPlayers(gameId: any, id: any):any{
+    const url = 'http://70.120.3.133:5000/game/' + gameId +"/playerOptions" ;
+    var jsonObj = {};
+    jsonObj["playerId"] = id;
+    jsonObj["ability"] = 1;
+    jsonObj["proficiency"] = 1;
+    return this.http.post(url, jsonObj);
+  }
+
+
+
 }
