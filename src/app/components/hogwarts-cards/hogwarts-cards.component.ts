eeacb610652689b3be9ef0b0b69cb442f8d35c6b
Rachel <rachelcramblitt@gmail.com>
2020-09-18 23:11:08 -0500

    for JJ lol

--- src/app/components/hogwarts-cards/hogwarts-cards.component.ts
+++ src/app/components/hogwarts-cards/hogwarts-cards.component.ts
@@ -9,7 +9,7 @@ export class HogwartsCardsComponent implements OnInit {
   hogwartsCards = [
     {name:'Lumos', type:'Spell', description:'All heroes draw a card'},
     {name:'Accio', type:'Spell', description:'Search your discard pile for any card under 5 coins and put it in your hand'},
-    {name:'Essence of Dettany', type:'Item', description:'Any one hero goes 2 hearts'},
+    {name:'Essence of Dittany', type:'Item', description:'Any one hero goes 2 hearts'},
     {name:'Wingardium Leviosa', type:'Spell', description:'Gain 1 coin. You may banish any card in your hand'},
     {name:'Quidditch Gear', type:'Item', description:'Gain 1 lightning and 1 heart'},
     {name:'Albus Dumbledore', type:'Ally', description:'All heroes gain 1 lightning, 1 heart, 1 coin, and draw a card'},
